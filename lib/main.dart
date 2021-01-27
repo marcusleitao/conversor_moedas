2bbe2d04f28bf5020676a5719b4638436826e5c7
Marcus Leitão <marcusvinicius.leitao@i9xp.com.br>
2021-01-26 23:09:35 -0300

    finalização do app

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,14 @@ const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=cab8db48";
 
 void main() async {
-
   print(await getData());
 
-  runApp(
-      MaterialApp(
-          home: Home()
-      )
-  );
+  runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+      )));
 }
 
 class Home extends StatefulWidget {
@@ -24,60 +24,107 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double dolar;
   double euro;
+
+  void _realChanged(String text) {
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Conversor de Moeda", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.amber,
-        centerTitle: true
-      ),
-      backgroundColor: Colors.black,
-      body: FutureBuilder<Map>(
-        future: getData(),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: Text(
-                    "Carregando Dados",
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 25.0
-                    ),
-                    textAlign: TextAlign.center
-                )
-              );
-            default:
-              if(snapshot.hasError){
-                return Center(
-                    child: Text(
-                        "Erro ao carregar dados :(",
-                        style: TextStyle(
+        appBar: AppBar(
+            title: Text("Conversor de Moeda",
+                style: TextStyle(color: Colors.white)),
+            backgroundColor: Colors.amber,
+            centerTitle: true),
+        backgroundColor: Colors.black,
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                      child: Text("Carregando Dados",
+                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                          textAlign: TextAlign.center));
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text("Erro ao carregar dados :(",
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                            textAlign: TextAlign.center));
+                  } else {
+                    dolar =
+                        snapshot.data['results']['currencies']['USD']['buy'];
+                    euro = snapshot.data['results']['currencies']['EUR']['buy'];
+                    return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(
+                            Icons.monetization_on,
+                            size: 150.0,
                             color: Colors.amber,
-                            fontSize: 25.0
-                        ),
-                        textAlign: TextAlign.center
-                    )
-                );
-              } else {
-                dolar = snapshot.data['results']['currencies']['USD']['buy'];
-                euro = snapshot.data['results']['currencies']['EUR']['buy'];
-                return Container(color: Colors.green);
+                          ),
+                          buildTextField(
+                              "Reais", "R\$ ", realController, _realChanged),
+                          Divider(),
+                          buildTextField("Dolares", "US\$ ", dolarController,
+                              _dolarChanged),
+                          Divider(),
+                          buildTextField(
+                              "Euros", "€ ", euroController, _euroChanged),
+                        ],
+                      ),
+                    );
+                  }
               }
-          }
-        }
-      )
-    );
+            }));
   }
 }
-
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix,
+    TextEditingController buildController, Function buildFunction) {
+  return TextField(
+    controller: buildController,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(
+      color: Colors.amber,
+      fontSize: 25.0,
+    ),
+    onChanged: buildFunction,
+  );
 }
